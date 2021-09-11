2bb7b60fcd533d7e70a8fc0f4d91446abc74f166
Doruk Akgül <87118358+dorukakgul@users.noreply.github.com>
2021-09-12 00:31:29 +0300

    Create odev12.sql

---
+++ odev12.sql
@@ -0,0 +1,14 @@
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+-- 	1.	film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+-- 	2.	film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+-- 	3.	film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+-- 	4.	payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+—1.
+
+select * from film where length >
+ (
+select avg(length) from film  );
+
+—2.
+SELECT COUNT(*) FROM FILM WHERE rental_rate = 4.99
